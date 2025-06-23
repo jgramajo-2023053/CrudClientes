398f284ced982e682747d48d867167e350f69d39
jgramajo-2023053 <jgramajo-2023053@kinal.edu.gt>
2025-06-23 12:53:37 -0600

    Simplificación del proyecto

--- ClienteApi/Data/ApplicationDbContext.cs
+++ ClienteApi/Data/ApplicationDbContext.cs
@@ -6,28 +6,8 @@ namespace ClienteApi.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
 
-        public DbSet<Cliente> Clientes { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Convertir nombres de tablas y columnas a minúsculas para evitar problemas con PostgreSQL
-            foreach (var entity in modelBuilder.Model.GetEntityTypes())
-            {
-                // Tabla en minúsculas
-                entity.SetTableName(entity.GetTableName().ToLower());
-
-                // Columnas en minúsculas
-                foreach (var property in entity.GetProperties())
-                {
-                    property.SetColumnName(property.GetColumnName().ToLower());
-                }
-            }
-        }
+        public DbSet<Cliente> Clientes => Set<Cliente>();
     }
 }
